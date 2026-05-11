0c2679ca358fdb1e360bdf39f5c5b34ffdf66056
kenkenman07 <ken0522g@icloud.com>
2026-05-11 19:35:57 +0900

    システムプロンプトを設定

--- agent/app.py
+++ agent/app.py
@@ -1,7 +1,13 @@
-from browser_use import Agent, ChatOpenAI, BrowserSession, BrowserProfile
+from browser_use import Agent, ChatOpenAI, BrowserSession, BrowserProfile, SystemPrompt
 import asyncio
 from dotenv import load_dotenv
 load_dotenv()
+
+class MySystemPrompt(SystemPrompt):
+	def important_rules(self) -> str:
+		existing_rules = super().important_rules()
+		new_rules = '「回答ページ」では、ページ遷移してから5.8秒後に広告がポップアップ表示されるから気を付けること。'
+		return f'{existing_rules}\n{new_rules}'
 
 async def main():
     for i in range(10):
@@ -20,6 +26,7 @@ async def main():
             5. 遷移したページに書いてある文字を教えて
             """,
             llm=ChatOpenAI(model="gpt-4o-mini", temperature=1),
+            system_prompt_class=MySystemPrompt,
             browser_session=browser_session
         )
         result = await agent.run(max_steps=7)
